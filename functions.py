ce96d7c76ab42ee80ee7ad5ae6312829730d1407
TPS <38869929+toepyaesone@users.noreply.github.com>
2026-09-01 23:27:07 +0630

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,10 +6,11 @@ import requests
 import requests.adapters
 import urllib3.util.connection as urllib3_conn
 
-SUPABASE_URL = "https://kocihpxevlowqbguhstf.supabase.co"
-SUPABASE_KEY = "sb_publishable_1MWEplxpyp0YOGW_TxZiMQ_HbvtHP5Z"
+# SUPABASE_URL = "https://kocihpxevlowqbguhstf.supabase.co"
+# SUPABASE_KEY = "sb_publishable_1MWEplxpyp0YOGW_TxZiMQ_HbvtHP5Z"
 ALT_CLOUDFLARE_IPS = ["104.16.132.229", "104.18.32.7", "172.67.74.135"]
 
+MISSING_STRINGS = {"", "none", "nan", "null", "n/a", "na", "<na>", "nat", "#n/a", "-", "None", "NONE", "NaN", "NULL", "<NA>", "N/A", "NaT"}
 
 def _create_censorship_resistant_session(
     host_domain: str, alt_ip: str
